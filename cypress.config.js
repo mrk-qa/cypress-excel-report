b7c4a0588a5ae518748a3e273083b565eb58ea3a
Marco Antonio <marco.ant.seg@gmail.com>
2023-12-26 18:26:45 -0300

    ajuste cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -10,13 +10,19 @@ module.exports = defineConfig({
 
       // Registro de tempo de início
       on('before:run', () => {
+        // Certificar-se de que o diretório existe
+        const dirPath = path.dirname(path.resolve(caminhoArquivo))
+        if (!fs.existsSync(dirPath)) {
+          fs.mkdirSync(dirPath, { recursive: true })
+        }
+
         // Limpar o arquivo antes de iniciar os testes
         fs.writeFileSync(path.resolve(caminhoArquivo), '')
       })
 
       // Registro de tempo de término e escrita do arquivo no final dos testes
       on('after:run', (results) => {
-        const jsonFilePath = path.join(__dirname, caminhoArquivo)
+        const jsonFilePath = path.resolve(caminhoArquivo)
 
         // Convertendo dados para formato JSON
         const dadosJSON = JSON.stringify(results, null, 2)
@@ -25,7 +31,7 @@ module.exports = defineConfig({
         fs.writeFileSync(jsonFilePath, dadosJSON)
 
         console.log(`Dados foram escritos em ${jsonFilePath}`)
-      })
+      });
 
       return config
     },

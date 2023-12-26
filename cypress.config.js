c2f37199763b7bcbd1c99f9cf53de3daa52559cf
Marco Antonio <marco.ant.seg@gmail.com>
2023-12-26 16:53:22 -0300

    ajuste no path

--- cypress.config.js
+++ cypress.config.js
@@ -1,33 +1,39 @@
-const { defineConfig } = require('cypress')
-const fs = require('fs')
-const path = require('path')
+const { defineConfig } = require('cypress');
+const fs = require('fs');
+const path = require('path');
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
 
-      const caminhoArquivo = 'cypress/reports/results_tests.json'
+      const caminhoArquivo = 'cypress/reports/results_tests.json';
 
       // Registro de tempo de início
       on('before:run', () => {
+        // Certificar-se de que o diretório existe
+        const dirPath = path.dirname(path.resolve(caminhoArquivo));
+        if (!fs.existsSync(dirPath)) {
+          fs.mkdirSync(dirPath, { recursive: true });
+        }
+
         // Limpar o arquivo antes de iniciar os testes
-        fs.writeFileSync(path.resolve(caminhoArquivo), '')
-      })
+        fs.writeFileSync(path.resolve(caminhoArquivo), '');
+      });
 
       // Registro de tempo de término e escrita do arquivo no final dos testes
       on('after:run', (results) => {
-        const jsonFilePath = path.join(__dirname, caminhoArquivo);
+        const jsonFilePath = path.resolve(caminhoArquivo);
 
         // Convertendo dados para formato JSON
-        const dadosJSON = JSON.stringify(results, null, 2)
+        const dadosJSON = JSON.stringify(results, null, 2);
 
         // Escrevendo dados no arquivo
-        fs.writeFileSync(jsonFilePath, dadosJSON)
+        fs.writeFileSync(jsonFilePath, dadosJSON);
 
-        console.log(`Dados foram escritos em ${jsonFilePath}`)
-      })
+        console.log(`Dados foram escritos em ${jsonFilePath}`);
+      });
 
-      return config
+      return config;
     },
   },
-})
+});

64fa6d0c5a18ccf4153ce817476d7362ed1a7a3c
Marco Antonio <marco.ant.seg@gmail.com>
2023-12-26 17:16:04 -0300

    ajuste do script

--- cypress/scripts/json-to-excel.js
+++ cypress/scripts/json-to-excel.js
@@ -1,12 +1,21 @@
-const ExcelJS = require('exceljs')
-const fs = require('fs')
-const { format } = require('date-fns')
-const path = require('path')
+const ExcelJS = require('exceljs');
+const fs = require('fs');
+const { format } = require('date-fns');
+const path = require('path');
 
-const jsonData = JSON.parse(fs.readFileSync('cypress/reports/results_tests.json', 'utf8'))
+// Construa o caminho relativo para o arquivo JSON
+const caminhoArquivoJson = path.join('cypress', 'reports', 'results_tests.json');
 
-const workbook = new ExcelJS.Workbook()
-const worksheet = workbook.addWorksheet('Cypress Test Results')
+// Verifique se o arquivo JSON existe
+if (!fs.existsSync(caminhoArquivoJson)) {
+  console.error(`O arquivo JSON não foi encontrado: ${caminhoArquivoJson}`);
+  process.exit(1); // Encerre o script com um código de erro
+}
+
+const jsonData = JSON.parse(fs.readFileSync(caminhoArquivoJson, 'utf8'));
+
+const workbook = new ExcelJS.Workbook();
+const worksheet = workbook.addWorksheet('Cypress Test Results');
 
 worksheet.addRow([
   'Spec',
@@ -15,7 +24,7 @@ worksheet.addRow([
   'Duration (ms)',
   'Cypress Version',
   'Browser',
-])
+]);
 
 jsonData.runs.forEach((run) => {
   run.tests.forEach((test) => {
@@ -26,32 +35,32 @@ jsonData.runs.forEach((run) => {
       test.duration,
       jsonData.cypressVersion,
       jsonData.browserName,
-    ])
-  })
-})
+    ]);
+  });
+});
 
 worksheet.addRow([
   'Total Passed',
   'Total Failed',
   'Total Duration',
   'Total Tests',
-])
+]);
 
 worksheet.addRow([
   jsonData.totalPassed,
   jsonData.totalFailed,
   jsonData.totalDuration,
   jsonData.totalTests,
-])
+]);
 
-const currentDate = format(new Date(), 'dd-MM-yyyy')
+const currentDate = format(new Date(), 'dd-MM-yyyy');
 
-const excelFilePath = path.join('cypress', 'reports', `cypress_results_${currentDate}.xlsx`)
+const excelFilePath = path.join('cypress', 'reports', `cypress_results_${currentDate}.xlsx`);
 
 workbook.xlsx.writeFile(excelFilePath)
   .then(() => {
-    console.log(`Planilha salva com sucesso: ${excelFilePath}`)
+    console.log(`Planilha salva com sucesso: ${excelFilePath}`);
   })
   .catch((err) => {
-    console.error('Erro ao salvar a planilha:', err)
-  })
+    console.error('Erro ao salvar a planilha:', err);
+  });

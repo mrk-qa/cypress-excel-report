d8405930ba437d06c15c1a8c369fbf78a7693ed5
Marco Antonio <marco.ant.seg@gmail.com>
2023-12-26 16:11:39 -0300

    removendo lib cypress-json-results e ajustando geração do json

--- cypress.config.js
+++ cypress.config.js
@@ -1,22 +1,31 @@
 const { defineConfig } = require('cypress')
-const CypressReport = require('cypress-json-results-non-blocking')
+const fs = require('fs')
+const path = require('path')
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      const cypressJSONReport = new CypressReport({ on, folder: 'cypress/reports', filename: 'results_tests.json' })
 
-      on('before:run', (details) => {
-        cypressJSONReport.beforeRunHandler(details)
+      const caminhoArquivo = 'cypress/reports/results_tests.json'
+
+      // Registro de tempo de início
+      on('before:run', () => {
+        // Limpar o arquivo antes de iniciar os testes
+        fs.writeFileSync(path.resolve(caminhoArquivo), '')
       })
 
-      on('after:spec', (spec, results) => {
-        cypressJSONReport.afterSpecWithDuration(spec, results)
-      })
-
+      // Registro de tempo de término e escrita do arquivo no final dos testes
       on('after:run', (results) => {
-        cypressJSONReport.afterRunHandler(results)
+        // Convertendo dados para formato JSON
+        const dadosJSON = JSON.stringify(results, null, 2)
+
+        // Escrevendo dados no arquivo
+        fs.writeFileSync(path.resolve(caminhoArquivo), dadosJSON)
+
+        console.log(`Dados foram escritos em ${caminhoArquivo}`)
       })
+
+      return config
     },
   },
 })
